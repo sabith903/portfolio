7ae39fa5db6a672c4cd28ec789fd8d5f9b6e0974
Sabith <sabithkarayil123@gmail.com>
2025-04-06 16:17:29 +0530

    hardcode host in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY =os.environ.get ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['sabithkarayil.in', 'www.sabithkarayil.in', '13.201.103.78']
 
 
 # Application definition
