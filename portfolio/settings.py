9da920914f8757eac02d0e5b2718a025642ee9de
Sabith <sabithkarayil123@gmail.com>
2025-04-07 10:38:15 +0530

    debug change to false

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ.get ('SECRET_KEY')
+SECRET_KEY =os.environ.get ('SECRET_KEY', 'django-insecure-76*c1uod6o#)vd4gfkureb#t+ow-qx6($aldiy#-ld!cx7@lq&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['sabithkarayil.in', 'www.sabithkarayil.in', '13.201.103.78']
 
